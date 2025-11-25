f6ef9e9b58725b772eb228c9e834e5c506e255fb
Arun Kumar Bidila <arunkumarbidila@gmail.com>
2025-11-25 23:34:04 +0530

    done with backend

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,7 +5,6 @@ import userModel from "../models/userModel.js";
 
 const placeOrder = async (req, res) => {
   try {
-
     const newOrder = new orderModel({
       userId: req.body.userId,
       items: req.body.items,
@@ -18,11 +17,41 @@ const placeOrder = async (req, res) => {
     await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
 
     res.json({ success: true, message: "Order Placed" });
-
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Error in placeOrder" });
   }
 };
 
-export { placeOrder };
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error in userOrders" });
+  }
+};
+
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error in listOrders" });
+  }
+};
+
+const updateStatus=async(req,res)=>{
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+    res.json({success:true,message:"Status updated"});
+    
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error in updateStatus" });
+  }
+}
+
+export { placeOrder, userOrders , listOrders,updateStatus};
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import {authMiddleware} from '../middleware/authMiddleware.js';
-import { placeOrder } from '../controllers/orderController.js';
+import { listOrders, placeOrder, updateStatus, userOrders } from '../controllers/orderController.js';
 const orderRouter=express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder);
+orderRouter.post("/userorders",authMiddleware,userOrders);
+orderRouter.get("/listorders",listOrders);
+orderRouter.post("/status",updateStatus);
 
 export default orderRouter;
